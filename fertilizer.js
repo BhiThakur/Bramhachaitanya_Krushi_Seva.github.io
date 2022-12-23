028607de3d7a7901525360a7b25d8a2e09159460
BhiThakur <121180818+BhiThakur@users.noreply.github.com>
2022-12-23 12:34:03 +0530

    readme

--- fertilizer.js
+++ fertilizer.js
@@ -1,12 +1,12 @@
-function Qut_for_ltr(){
-    var ChemiName = document.getElementById("chemicle_product").value ;
-    var ChemiQut = document.getElementById("chemi_qut").value ;
+function Qut_for_ltr() {
+    var ChemiName = document.getElementById("chemicle_product").value;
+    var ChemiQut = document.getElementById("chemi_qut").value;
     var message = "";
     var One_lr_mrp = "";
     var fiveHundrade_ml_mrp = "";
     var five_lr_mrp = "";
     var tenlr_mrp = "";
-    var onelr_cp= "";
+    var onelr_cp = "";
     var fiveHundrade_ml_cp = "";
     var five_lr_cp = "";
     var ten_lr_cp = "";
@@ -19,20 +19,206 @@ function Qut_for_ltr(){
     var qut7 = "";
     var qut8 = "";
 
-    
-   if( ChemiName == "00:52:34" && ChemiQut < 200) {
+
+    if (ChemiName == "00:52:34" && ChemiQut < 200) {
         message = ChemiQut * 1 / 200 * 1000 + " gm";
-        One_lr_mrp = "₹2950";
-        fiveHundrade_ml_mrp = "-";
-        onelr_cp= "₹1700";
-        fiveHundrade_ml_cp = "-";
+        // 1kg mrp
+        One_lr_mrp = "₹370";
+        // 1kg cp
+        onelr_cp = "₹260";
+        // 25kg mrp
+        tenlr_mrp = "₹6475";
+        // 25kg cp
+        ten_lr_cp = "₹5650";
 
         five_lr_mrp = "-";
-        tenlr_mrp = "-";
-        onelr_cp= "₹1700";
-        fiveHundrade_ml_cp = "-";
+        fiveHundrade_ml_mrp = "-";
         five_lr_cp = "-";
-        ten_lr_cp = "-";
+        fiveHundrade_ml_cp = "-";
+
+        qut1 = "1kg";
+        qut2 = "5kg";
+        qut3 = "15kg";
+        qut4 = "25kg";
+        qut5 = "1kg";
+        qut6 = "5kg";
+        qut7 = "15kg";
+        qut8 = "25kg";
+    }
+
+    else if (ChemiName == "00:52:34" && ChemiQut >= 200) {
+        message = ChemiQut * 1 / 200 + " kg";
+
+        // 1kg mrp
+        One_lr_mrp = "₹370";
+        // 1kg cp
+        onelr_cp = "₹260";
+        // 25kg mrp
+        tenlr_mrp = "₹6475";
+        // 25kg cp
+        ten_lr_cp = "₹5650";
+
+        five_lr_mrp = "-";
+        fiveHundrade_ml_mrp = "-";
+        five_lr_cp = "-";
+        fiveHundrade_ml_cp = "-";
+
+        qut1 = "1kg";
+        qut2 = "5kg";
+        qut3 = "15kg";
+        qut4 = "25kg";
+        qut5 = "1kg";
+        qut6 = "5kg";
+        qut7 = "15kg";
+        qut8 = "25kg";
+    }
+
+    else if (ChemiName == "13:40:13" && ChemiQut < 200) {
+        message = ChemiQut * 1 / 200 * 1000 + " gm";
+        // 1kg mrp
+        One_lr_mrp = "₹320";
+        // 1kg cp
+        onelr_cp = "₹250";
+        // 25kg mrp
+        tenlr_mrp = "₹4900";
+        // 25kg cp
+        ten_lr_cp = "₹4250";
+
+        five_lr_mrp = "-";
+        fiveHundrade_ml_mrp = "-";
+        five_lr_cp = "-";
+        fiveHundrade_ml_cp = "-";
+
+        qut1 = "1kg";
+        qut2 = "5kg";
+        qut3 = "15kg";
+        qut4 = "25kg";
+        qut5 = "1kg";
+        qut6 = "5kg";
+        qut7 = "15kg";
+        qut8 = "25kg";
+    }
+
+    else if (ChemiName == "13:40:13" && ChemiQut >= 200) {
+        message = ChemiQut * 1 / 200 + " kg";
+
+        // 1kg mrp
+        One_lr_mrp = "₹320";
+        // 1kg cp
+        onelr_cp = "₹250";
+        // 25kg mrp
+        tenlr_mrp = "₹4900";
+        // 25kg cp
+        ten_lr_cp = "₹4250";
+
+        five_lr_mrp = "-";
+        fiveHundrade_ml_mrp = "-";
+        five_lr_cp = "-";
+        fiveHundrade_ml_cp = "-";
+
+        qut1 = "1kg";
+        qut2 = "5kg";
+        qut3 = "15kg";
+        qut4 = "25kg";
+        qut5 = "1kg";
+        qut6 = "5kg";
+        qut7 = "15kg";
+        qut8 = "25kg";
+    }
+    else if (ChemiName == "13:00:45" && ChemiQut < 200) {
+        message = ChemiQut * 1 / 200 * 1000 + " gm";
+        // 1kg mrp
+        One_lr_mrp = "₹380";
+        // 1kg cp
+        onelr_cp = "₹200";
+        // 25kg mrp
+        tenlr_mrp = "₹4920";
+        // 25kg cp
+        ten_lr_cp = "₹4250";
+
+        five_lr_mrp = "-";
+        fiveHundrade_ml_mrp = "-";
+        five_lr_cp = "-";
+        fiveHundrade_ml_cp = "-";
+
+        qut1 = "1kg";
+        qut2 = "5kg";
+        qut3 = "15kg";
+        qut4 = "25kg";
+        qut5 = "1kg";
+        qut6 = "5kg";
+        qut7 = "15kg";
+        qut8 = "25kg";
+    }
+
+    else if (ChemiName == "13:00:45" && ChemiQut >= 200) {
+        message = ChemiQut * 1 / 200 + " kg";
+
+        // 1kg mrp
+        One_lr_mrp = "₹380";
+        // 1kg cp
+        onelr_cp = "₹200";
+        // 25kg mrp
+        tenlr_mrp = "₹4920";
+        // 25kg cp
+        ten_lr_cp = "₹4250";
+
+        five_lr_mrp = "-";
+        fiveHundrade_ml_mrp = "-";
+        five_lr_cp = "-";
+        fiveHundrade_ml_cp = "-";
+
+        qut1 = "1kg";
+        qut2 = "5kg";
+        qut3 = "15kg";
+        qut4 = "25kg";
+        qut5 = "1kg";
+        qut6 = "5kg";
+        qut7 = "15kg";
+        qut8 = "25kg";
+    }
+    else if (ChemiName == "19:19:19" && ChemiQut < 200) {
+        message = ChemiQut * 1 / 200 * 1000 + " gm";
+        // 1kg mrp
+        One_lr_mrp = "₹280";
+        // 1kg cp
+        onelr_cp = "₹200";
+        // 25kg mrp
+        tenlr_mrp = "₹4100";
+        // 25kg cp
+        ten_lr_cp = "₹3650";
+
+        five_lr_mrp = "-";
+        fiveHundrade_ml_mrp = "-";
+        five_lr_cp = "-";
+        fiveHundrade_ml_cp = "-";
+
+        qut1 = "1kg";
+        qut2 = "5kg";
+        qut3 = "15kg";
+        qut4 = "25kg";
+        qut5 = "1kg";
+        qut6 = "5kg";
+        qut7 = "15kg";
+        qut8 = "25kg";
+    }
+
+    else if (ChemiName == "19:19:19" && ChemiQut >= 200) {
+        message = ChemiQut * 1 / 200 + " kg";
+
+        // 1kg mrp
+        One_lr_mrp = "₹280";
+        // 1kg cp
+        onelr_cp = "₹200";
+        // 25kg mrp
+        tenlr_mrp = "₹4100";
+        // 25kg cp
+        ten_lr_cp = "₹3650";
+
+        five_lr_mrp = "-";
+        fiveHundrade_ml_mrp = "-";
+        five_lr_cp = "-";
+        fiveHundrade_ml_cp = "-";
 
         qut1 = "1kg";
         qut2 = "5kg";
@@ -44,19 +230,21 @@ function Qut_for_ltr(){
         qut8 = "25kg";
     }
     
-    else if( ChemiName == "00:52:34" && ChemiQut >= 200) {
-        message = ChemiQut * 1 / 200 + " kg";
-        One_lr_mrp = "₹2950";
-        fiveHundrade_ml_mrp = "-";
-        onelr_cp= "₹1700";
-        fiveHundrade_ml_cp = "-";
+    else if (ChemiName == "00:00:50" && ChemiQut < 200) {
+        message = ChemiQut * 1 / 200 * 1000 + " gm";
+        // 1kg mrp
+        One_lr_mrp = "-";
+        // 1kg cp
+        onelr_cp = "-";
+        // 25kg mrp
+        tenlr_mrp = "₹4100";
+        // 25kg cp
+        ten_lr_cp = "₹2175";
 
         five_lr_mrp = "-";
-        tenlr_mrp = "-";
-        onelr_cp= "₹1700";
-        fiveHundrade_ml_cp = "-";
+        fiveHundrade_ml_mrp = "-";
         five_lr_cp = "-";
-        ten_lr_cp = "-";
+        fiveHundrade_ml_cp = "-";
 
         qut1 = "1kg";
         qut2 = "5kg";
@@ -67,9 +255,116 @@ function Qut_for_ltr(){
         qut7 = "15kg";
         qut8 = "25kg";
     }
+
+    else if (ChemiName == "00:00:50" && ChemiQut >= 200) {
+        message = ChemiQut * 1 / 200 + " kg";
+
+       // 1kg mrp
+       One_lr_mrp = "-";
+       // 1kg cp
+       onelr_cp = "-";
+       // 25kg mrp
+       tenlr_mrp = "₹4100";
+       // 25kg cp
+       ten_lr_cp = "₹2175";
+
+        five_lr_mrp = "-";
+        fiveHundrade_ml_mrp = "-";
+        five_lr_cp = "-";
+        fiveHundrade_ml_cp = "-";
+
+        qut1 = "1kg";
+        qut2 = "5kg";
+        qut3 = "15kg";
+        qut4 = "25kg";
+        qut5 = "1kg";
+        qut6 = "5kg";
+        qut7 = "15kg";
+        qut8 = "25kg";
+    }
+    else if (ChemiName == "12:61:00" && ChemiQut < 200) {
+        message = ChemiQut * 1 / 200 * 1000 + " gm";
+        // 1kg mrp
+        One_lr_mrp = "-";
+        // 1kg cp
+        onelr_cp = "-";
+        // 25kg mrp
+        tenlr_mrp = "₹4950";
+        // 25kg cp
+        ten_lr_cp = "₹4165";
+
+        five_lr_mrp = "-";
+        fiveHundrade_ml_mrp = "-";
+        five_lr_cp = "-";
+        fiveHundrade_ml_cp = "-";
+
+        qut1 = "1kg";
+        qut2 = "5kg";
+        qut3 = "15kg";
+        qut4 = "25kg";
+        qut5 = "1kg";
+        qut6 = "5kg";
+        qut7 = "15kg";
+        qut8 = "25kg";
+    }
+
+    else if (ChemiName == "12:61:00" && ChemiQut >= 200) {
+        message = ChemiQut * 1 / 200 + " kg";
+
+        // 1kg mrp
+        One_lr_mrp = "-";
+        // 1kg cp
+        onelr_cp = "-";
+        // 25kg mrp
+        tenlr_mrp = "₹4950";
+        // 25kg cp
+        ten_lr_cp = "₹4165";
+
+        five_lr_mrp = "-";
+        fiveHundrade_ml_mrp = "-";
+        five_lr_cp = "-";
+        fiveHundrade_ml_cp = "-";
+
+        qut1 = "1kg";
+        qut2 = "5kg";
+        qut3 = "15kg";
+        qut4 = "25kg";
+        qut5 = "1kg";
+        qut6 = "5kg";
+        qut7 = "15kg";
+        qut8 = "25kg";
+    }
+
+    else if (ChemiName == "Borofoll") {
+        message = ChemiQut * 1 + " gm";
+
+        // 1kg mrp
+        One_lr_mrp = "₹540";
+        // 1kg cp
+        onelr_cp = "₹350";
+        // 25kg mrp
+        tenlr_mrp = "-";
+        // 25kg cp
+        ten_lr_cp = "-";
+
+        five_lr_mrp = "-";
+        fiveHundrade_ml_mrp = "-";
+        five_lr_cp = "-";
+        fiveHundrade_ml_cp = "-";
+
+        qut1 = "1kg";
+        qut2 = "5kg";
+        qut3 = "15kg";
+        qut4 = "25kg";
+        qut5 = "1kg";
+        qut6 = "5kg";
+        qut7 = "15kg";
+        qut8 = "25kg";
+    }
+
 
     else {
-    message = "-"
+        
     }
     document.getElementById("chemi_ans").textContent = message;
     document.getElementById("1lr_mrp").textContent = One_lr_mrp;
